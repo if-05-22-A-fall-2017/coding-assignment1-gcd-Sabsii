13fdadcc052c98a0c2be0f5f007e45f992e1af8f
Sabsii <sabida@gmx.at>
2018-02-04 12:14:36 +0100

    new

--- gcd/src/gcd/Gcd.java
+++ gcd/src/gcd/Gcd.java
@@ -5,6 +5,10 @@
  */
 package gcd;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author dalpi
@@ -22,14 +26,56 @@ public class Gcd {
 
     }
     static int gcdPrimeFactors(int a, int b) {
-        while(a != b){
-            if(a > b)
-                a = a - b;
-            else
-                b = b - a;
+        List<Integer> primForA = primeFactors(a);
+        List<Integer> primForB = primeFactors(b);
+        
+        List<Integer> commonPrim = getCommon(primForA, primForB);
+       
+        
+        int tmp = commonPrim.get(0);
+        for(int i = 0; i < commonPrim.size()-1; i++)
+        {
+            tmp *= commonPrim.get(i+1);
         }
-        return a;
+        return tmp;
     }
+    
+    static  List<Integer> primeFactors(int number) {
+        int n = number;
+        List<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+        return factors;
+    }
+    
+    static List<Integer> getCommon(List<Integer> primForA, List<Integer> primForB) {
+        List<Integer> bigger;
+        List<Integer> smaller;
+        List<Integer> common = new ArrayList<Integer>();
+        if(primForA.size() < primForB.size()){
+            bigger = primForB;
+            smaller = primForA;
+        }
+        else{
+            bigger = primForA;
+            smaller = primForB;
+        }
+        for (int i = 0; i < smaller.size(); i++) {
+            if(bigger.get(i) == smaller.get(i)){
+                common.add(bigger.get(i));
+            }
+        }
+         if(common.size() == 0){
+            List<Integer> bla = Arrays.asList(1);
+            return bla;
+        }
+        return common;
+    }
+    
     static int gcdEucledian(int a, int b) {
         if(b == 0){
             return a;
